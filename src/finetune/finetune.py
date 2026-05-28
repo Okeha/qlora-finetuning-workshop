7c0516216c51fba7a2948432502beaf90800be1e
okeha <okehanthony1234@gmail.com>
2026-05-29 00:27:16 +0100

    chore: Generic params initialization and script draft for finetune and infer pipelines

---
+++ src/finetune/finetune.py
@@ -0,0 +1,9 @@
+# Usage:
+# Run the fine-tuning pipeline using the provided dataset and configuration.
+# The script will handle the entire fine-tuning process, including:
+# - Loading the base model and tokenizer.
+# - Preparing the dataset for training.
+# - Configuring the training parameters.
+# - Executing the training loop.
+# - Saving the fine-tuned model for later use in inference.
+# - Configuring logging and monitoring for the training process.
---
+++ src/inference/infer.py
@@ -0,0 +1,7 @@
+# Simple Script to Run Inference using the fine-tuned model
+
+# Usage:
+# Load QLoRA Finetuned Model.
+# Accept Input Via Terminal (prompt for user input).
+# Generate output and print to terminal.
+
